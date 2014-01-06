8f4420e1b11414086001d43cb2b506227cb0341d
Jim Graham <jim@jim-graham.net>
2014-01-06 16:01:56 -0500

    Add default Chocolatey install
    Install Chocolatey and default programs for development windows machine

---
+++ Chocolatey/installDefaults.ps1
@@ -0,0 +1,41 @@
+#-- remember to run as Admin, and set PS execution policy
+# Set-ExecutionPolicy Unrestricted
+
+#-- Chocolatey itself
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'));
+
+#-- Install applications
+cinst 7zip
+cinst 7zip.commandline
+cinst paint.net
+cinst dropbox
+cinst CutePDF 
+cinst GoogleChrome 
+cinst TortoiseGit 
+cinst tortoisesvn
+cinst AnkhSvn 
+cinst GitHub 
+cinst 1password 
+cinst HipChat 
+cinst VirtualCloneDrive 
+cinst imgburn 
+cinst Firefox 
+cinst thunderbird 
+cinst nsis 
+cinst windirstat 
+cinst notepadplusplus 
+cinst pidgin 
+cinst snoop 
+cinst diffmerge 
+cinst teamviewer 
+cinst windirstat 
+cinst dotPeek
+cinst fiddler 
+cinst reshack
+cinst OpenOffice
+cinst SharpKeys
+
+#-- Currently fail
+# cinst IcoFx 
+# cinst FoxitReader
+# cinst ActivePerl 
